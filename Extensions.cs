6466ad7318e0786069ffd153b4d06fcde560f9a4
AReeme <areeme@student.neumont.edu>
2024-01-14 17:23:53 -0700

    Added Chess 960 Option to XAML and a Shuffling Method

--- Extensions.cs
+++ Extensions.cs
@@ -28,5 +28,20 @@ namespace Chess
         {
             return new Point(files.IndexOf(coordinate[0]), ranks.IndexOf(coordinate[1]));
         }
+
+        //Shuffling Pieces Algorithm
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            Random rng = new Random();
+            int n = list.Count;
+            while (n > 1) 
+            {
+                n--;
+                int k = rng.Next(n);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
     }
 }
